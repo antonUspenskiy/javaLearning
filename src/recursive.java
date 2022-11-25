47927b6c0be72f5a3f73baad8822af069fcdd083
Anton Uspenskiy <uspenskii24@gmail.com>
2022-11-25 10:05:14 +0300

    java lessons

--- src/recursive.java
+++ src/recursive.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class newLesson {
+public class recursive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите число: \n");
